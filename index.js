8d6cc333e73c1d4de02be0347dd0010fc877ddc3
10Sadique <jafarsjahan@gmail.com>
2022-10-20 14:24:55 +0600

    catergory-wise news & news api endpoint added

--- index.js
+++ index.js
@@ -4,15 +4,32 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send({ message: 'Hello World' });
+    res.send({ message: 'This is Bangla Times Server.' });
 });
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    } else {
+        const catagoryNews = news.filter((n) => n.category_id === id);
+        res.send(catagoryNews);
+    }
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find((n) => n._id === id);
+    res.send(selectedNews);
 });
 
 app.listen(port, () => {
